a9a52f24f59862a7d62d20896108911477200552
tohow06 <k82733228@gmail.com>
2023-07-25 02:56:08 +0800

    Modi: mesh_master

--- mesh_example_master.py
+++ mesh_example_master.py
@@ -2,13 +2,17 @@
 Example of using the rf24_mesh module to operate the nRF24L01 transceiver as
 a Mesh network master node.
 """
+import struct
 from pyrf24 import RF24, RF24Network, RF24Mesh
+from pyrf24 import RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
 
-
-radio = RF24(22, 0)
+radio = RF24(22, 0, 1000000)
 network = RF24Network(radio)
 mesh = RF24Mesh(radio, network)
 mesh.node_id = 0
+radio.begin()
+radio.set_pa_level(RF24_PA_MIN, 0)
+
 if not mesh.begin():
     # if mesh.begin() returns false for a master node,
     # then radio.begin() returned false.
@@ -22,7 +26,10 @@ try:
 
         while network.available():
             header, payload = network.read()
-            print(f"Received message {header.to_string()}")
+            data = int.from_bytes(payload, "little")
+            print(f"Received message {header.to_string()} data:{data}")
+            print(f"from_node_id = {mesh.get_node_id(header.from_node)}")
+
 except KeyboardInterrupt:
     print("powering down radio and exiting.")
     radio.power = False
